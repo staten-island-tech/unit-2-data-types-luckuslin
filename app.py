0346a295fd30f8923fbf40a0a75325fa84002a99
luckuslin <lukasl18@nycstudents.net>
2026-02-04 09:22:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-def discount(age,resident,member):
-
- age= input("what is your age?")
- if age >=12:
+def discount(child,resident,member):
+  
+ resident=input("Are you a citizen?")
+ if resident== "Yes":
   print (True)
- else : 
+ else :
+  print (False)
+  
+  member=input("Do you have an active Membership?")
+  if member == "I have an Active Membership":
+    print(True) 
+  else : 
     print (False)
-resident=input("Are you a citizen?")
+
+  child=input("How Old are You?")
+  if child == range(1-12) or range (65-100):
+    print(True)
+  else :
+    print (False)
+    
+    if resident and member and child == (True):
+      print ("discount")
+    else :
+      print ("PAY UP")
+      
 
 
 discount
