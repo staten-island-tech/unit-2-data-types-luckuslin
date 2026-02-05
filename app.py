2aff7a6560490c1d88ef79ec67f38acdf376ff47
luckuslin <lukasl18@nycstudents.net>
2026-02-05 09:26:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-def discount(child,resident,member):
-  
- resident=input("Are you a citizen?")
- if resident== "Yes":
-  print (True)
- else :
-  print (False)
-  
-  member=input("Do you have an active Membership?")
-  if member == "I have an Active Membership":
-    print(True) 
-  else : 
-    print (False)
-
-  child=input("How Old are You?")
-  if child == range(1-12) or range (65-100):
-    print(True)
-  else :
-    print (False)
-    
-    if resident and member and child == (True):
-      print ("discount")
+""" def discount(child,resident,member):
+    resident=input("Are you a citizen?")
+    if resident == ("Yes"):
+    print ("True")
     else :
-      print ("PAY UP")
-      
+    print ("False")
+    
+child=input("How Old Are You?")
+   
+if child == range(1-12) or range (65-100):
+     print(True)
+else :
+     print (False)
+     
+member=input("Do you have an active Membership?") 
+
+if member == ("Yes"):
+    
+    print(True) 
+else : 
+    print (False) """
+  
 
 
-discount
+def Odd_or_Even():
+    x=2
+    Odd=[1+x]
+    Even=x%2]
